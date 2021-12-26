df6004546653a52efe408e40e19f86c9de88ef73
RafaPCHome <rafaassis15@gmail.com>
2021-12-26 11:48:16 -0300

    alter env

---
+++ src/env/load_env.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # Load from './.env
+
+TEST_ENV = os.getenv('DB_HOST') # Retorna None se nao encontrar
+
+if(TEST_ENV == None):
+    raise Exception('ENV Variables NOT FOUND')
+
+DB_HOST    = os.getenv('DB_HOST')
+DB_USER    = os.getenv('DB_USER')
+DB_PORT    = int(os.getenv('DB_PORT'))
+DB_PASS    = os.getenv('DB_PASS')
+DB_NAME    = os.getenv('DB_NAME')
+DB_TABLE   = os.getenv('DB_TABLE')
